3450562a732c49f69da1d3824748e3124e222897
Benedikt Bertsch <benedikt.bertsch@gmail.com>
2020-08-17 16:45:33 +0200

    added testfile and made it easier to maintain

--- src/main.go
+++ src/main.go
@@ -2,43 +2,51 @@ package main
 
 import (
 	"strconv"
-	"github.com/go-resty/resty/v2"
 	"os"
 	"strings"
 	"encoding/json"
 	"time"
 	"flag"
 	"fmt"
+	"net/http"
+	"bytes"
 )
 
-//Global variables (Environment)
+///Global variables (Environment)
+
+//TOKENS = Bearer Token for authorization
 var TOKENS []string
-var MAILS []string
+//ZONES = Predefined Cloudflare DNS Zone
 var ZONES []string
+//DOMAINS = Domain ex: test.com
 var DOMAINS []string
+//PROXIES = if the traffic should get proxied by Cloudflare
 var PROXIES []bool
+//IPV6 = if you want to update IPv6 as well
 var IPV6 []bool
+//INTERVAL = Minuteinterval in which the DNS should get updated
 var INTERVAL uint64
 
+//PREVIOUSIP4 = The previous used IPv4
+var PREVIOUSIP4 string
+//PREVIOUSIP6 = The previous used IPv4
+var PREVIOUSIP6 string
+
 func main() {
-	var runonce bool
 	var ticker *time.Ticker
-	runEnv()
-	CheckDuration := flag.Duration("duration", time.Duration(INTERVAL), "update interval (ex. 15s, 1m, 6h); if not specified or set to 0s, run only once and exit")
-	flag.Parse()
-	if *CheckDuration == time.Duration(0) {
-		runonce = true
-	} else {
+	//Only for debugging purposes or if you want to run without environment variables
+	//setEnvVariables()
+	if checkConfig(){
+		runEnv()
+		CheckDuration := flag.Duration("duration", time.Duration(INTERVAL), "update interval (ex. 15s, 1m, 6h); if not specified or set to 0s, run only once and exit")
+		flag.Parse()
 		ticker = time.NewTicker(*CheckDuration*time.Minute)
-	}
-	runddns()
-
-	if runonce {
-		os.Exit(0)
-	}
-
-	for range ticker.C {
+	
 		runddns()
+	
+		for range ticker.C {
+			runddns()
+		}
 	}
 }
 
@@ -49,7 +57,6 @@ func runEnv(){
 
 func setEnvVariables(){
 	os.Setenv("CF_TOKENS", "")
-	os.Setenv("CF_MAILS", "")
 	os.Setenv("CF_ZONES", "")
 	os.Setenv("CF_DOMAINS", "")
 	os.Setenv("CF_PROXIES", "")
@@ -58,37 +65,41 @@ func setEnvVariables(){
 }
 
 func debugEnvVariables(){
-	println(os.Getenv("CF_TOKENS"))
-	println(os.Getenv("CF_MAILS"))
-	println(os.Getenv("CF_ZONES"))
-	println(os.Getenv("CF_DOMAINS"))
-	println(os.Getenv("CF_PROXIES"))
-	println(os.Getenv("CF_IPV6"))
-	println(os.Getenv("CF_INTERVAL"))
+	fmt.Println(os.Getenv("CF_TOKEN"))
+	fmt.Println(os.Getenv("CF_ZONES"))
+	fmt.Println(os.Getenv("CF_DOMAINS"))
+	fmt.Println(os.Getenv("CF_PROXIES"))
+	fmt.Println(os.Getenv("CF_IPV6"))
+	fmt.Println(os.Getenv("CF_INTERVAL"))
 }
 
 func splitEnvVariables(){
 	TOKENS = strings.Split(os.Getenv("CF_TOKENS"), ",")
-	MAILS = strings.Split(os.Getenv("CF_MAILS"), ",")
 	ZONES = strings.Split(os.Getenv("CF_ZONES"), ",")
 	DOMAINS = strings.Split(os.Getenv("CF_DOMAINS"), ",")
-	var temp = strings.Split(os.Getenv("CF_PROXIES"), ",")
+	temp := strings.Split(os.Getenv("CF_PROXIES"), ",")
 	for i := 0; i < len(temp); i++ {
 		b, err := strconv.ParseBool(temp[i])
-		if err != nil{
-			println(err)
+		if err != nil {
+			PROXIES = append(PROXIES, false)
+		} else {
+			PROXIES = append(PROXIES, b)
 		}
-		PROXIES = append(PROXIES,b)
 	}
 	temp = strings.Split(os.Getenv("CF_IPV6"), ",")
 	for i := 0; i < len(temp); i++ {
 		b, err := strconv.ParseBool(temp[i])
-		if err != nil{
-			println(err)
+		if err != nil {
+			IPV6 = append(IPV6, false)
+		} else {
+			IPV6 = append(IPV6, b)
 		}
-		IPV6 = append(IPV6,b)
 	}
 	INTERVAL, _ = strconv.ParseUint(os.Getenv("CF_INTERVAL"), 10, 64)
+	_, err := strconv.ParseUint(os.Getenv("CF_INTERVAL"), 10, 64)
+	if err != nil {
+		INTERVAL = 1
+	}
 }
 
 func runddns() {
@@ -96,64 +107,103 @@ func runddns() {
 	var ipv4 = getAddressIpv4()
 	var ipv6 = getAddressIpv6()
 	//Loop over all Cloudflare data
-    fmt.Println("Checking for updates:", time.Now().Format("15.01.2006 15:04:05"))
+	//First check if ENV data are set
+	fmt.Println("Checking for updates:", time.Now().Format("15.01.2006 15:04:05"))
 	for i := 0; i < len(TOKENS); i++ {
-		var IDa = checkUpdate("A", ipv4, DOMAINS[i], ZONES[i], MAILS[i], TOKENS[i])
+		var IDa = checkUpdate("A", ipv4, DOMAINS[i], ZONES[i], TOKENS[i])
 		if IDa != "" {
-			update(ZONES[i], IDa, MAILS[i], TOKENS[i], ipv4, PROXIES[i], DOMAINS[i], "A")
+			update(ZONES[i], IDa, TOKENS[i], ipv4, PROXIES[i], DOMAINS[i], "A", PREVIOUSIP4)
 		}else {
-			print("IPv4 of " + DOMAINS[i] + " is still the same.")
+			fmt.Println("IPv4 of " + DOMAINS[i] + " is still the same.")
 		}
 		if IPV6[i] {
-			var IDaaaa = checkUpdate("AAAA", ipv6, DOMAINS[i], ZONES[i], MAILS[i], TOKENS[i])
+			var IDaaaa = checkUpdate("AAAA", ipv6, DOMAINS[i], ZONES[i], TOKENS[i])
 			if IDaaaa != "" {
-				update(ZONES[i], IDaaaa, MAILS[i], TOKENS[i], ipv6, PROXIES[i], DOMAINS[i], "AAAA")
+				update(ZONES[i], IDaaaa, TOKENS[i], ipv6, PROXIES[i], DOMAINS[i], "AAAA", PREVIOUSIP6)
 			} else {
-				println("IPv6 of " + DOMAINS[i] + " is still the same.")
+				fmt.Println("IPv6 of " + DOMAINS[i] + " is still the same.")
 			}
 		}
 	}
 }
 
-func update(zone string, id string, mail string, token string, ip string, proxy bool, domain string, recordtype string){
-	client := resty.New()
-	var status = recordUpdate{}
-	resp, _ := client.R().SetHeaders(map[string]string{
-        "Content-Type": "application/json",
-		"X-Auth-Email": mail,
-		"X-Auth-Key": token,
-	  }).SetBody(`{
+func checkConfig() bool {
+	if os.Getenv("CF_TOKENS") == ""{
+		fmt.Println("No CF_TOKENS set. This parameter is needed.")
+		return false
+	}
+	if os.Getenv("CF_ZONES") == ""{
+		fmt.Println("No CF_ZONES set. This parameter is needed.")
+		return false
+	}
+	if os.Getenv("CF_DOMAINS") == ""{
+		fmt.Println("No CF_DOMAINS set. This parameter is needed.")
+		return false
+	}
+	if os.Getenv("CF_PROXIES") == ""{
+		fmt.Println("No CF_PROXIES set. Will use default: false")
+	}
+	if os.Getenv("CF_IPV6") == ""{
+		fmt.Println("No CF_IPV6 set. Will use default: false")
+	}
+	if os.Getenv("CF_INTERVAL") == ""{
+		fmt.Println("No CF_INTERVAL set. Will use default: 1")
+	}
+	return true
+}
+
+func update(zone string, id string, token string, ip string, proxy bool, domain string, recordtype string, previousip string){
+	status := recordUpdate{}
+	body := []byte(`{
 		"type": "` + recordtype + `",
 		"name": "` + domain + `",
 		"content": "` + ip + `",
 		"ttl": 120,
 		"proxied": ` + strconv.FormatBool(proxy) + `
-	}`).Put("https://api.cloudflare.com/client/v4/zones/" + zone + "/dns_records/" + id)
-	err := json.Unmarshal(resp.Body(), &status)
+	}`)
+	req, err := http.NewRequest("PUT", "https://api.cloudflare.com/client/v4/zones/" + zone + "/dns_records/" + id, bytes.NewBuffer(body))
+	req.Header.Add("Authorization", "Bearer " + token)
+	client := &http.Client{}
+	resp, err := client.Do(req)
 	if err != nil {
-        println(err)
+        fmt.Println(err)
+	}
+	defer resp.Body.Close()
+	decoder := json.NewDecoder(resp.Body)
+	err = decoder.Decode(&status)
+	if err != nil {
+        fmt.Println(err)
 	}
 	if status.Success {
-		println("Domain: " + domain + " got updated with IP: " + ip)
+		fmt.Println("Domain: " + domain + " got updated with IP: " + ip + " Prvious IP: " + previousip)
 	} else {
-		println("Error on updating the IP: " + status.Errors[0].Message)
+		fmt.Println("Error on updating the IP: " + status.Errors[0].Message)
 	}
 }
 
-func checkUpdate(recordtype string, currentIP string, domain string, zone string, mail string, token string) string {
-	client := resty.New()
-	var record = records{}
-	resp, _  := client.R().SetHeaders(map[string]string{
-        "Content-Type": "application/json",
-		"X-Auth-Email": mail,
-		"X-Auth-Key": token,
-	  }).Get("https://api.cloudflare.com/client/v4/zones/" + zone + "/dns_records?type=" + recordtype)
-	err := json.Unmarshal(resp.Body(), &record)
+func checkUpdate(recordtype string, currentIP string, domain string, zone string, token string) string {
+	record := records{}
+	req, err := http.NewRequest("GET", "https://api.cloudflare.com/client/v4/zones/" + zone + "/dns_records?type=" + recordtype, nil)
+	req.Header.Add("Authorization", "Bearer " + token)
+	client := &http.Client{}
+	resp, err := client.Do(req)
 	if err != nil {
-        println(err)
+        fmt.Println(err)
+	}
+	defer resp.Body.Close()
+	decoder := json.NewDecoder(resp.Body)
+	err = decoder.Decode(&record)
+	if err != nil {
+        fmt.Println(err)
 	}
 	for i := 0; i < len(record.Result); i++ {
 		if record.Result[i].Content != currentIP && record.Result[i].Name == domain{
+			switch recordtype {
+			case "A": PREVIOUSIP4 = record.Result[i].Content
+				break;
+			case "AAAA": PREVIOUSIP6 = record.Result[i].Content
+				break;
+			}
 			return record.Result[i].ID
 		}
 	}
@@ -161,23 +211,31 @@ func checkUpdate(recordtype string, currentIP string, domain string, zone string
 }
 
 func getAddressIpv4() string{
-	client := resty.New()
 	address := address{}
-	resp, _  := client.R().Get("https://api.ipify.org?format=json")
-	err := json.Unmarshal(resp.Body(), &address)
+	resp, err  := http.Get("https://api.ipify.org?format=json")
 	if err != nil {
-        println(err)
+        fmt.Println(err)
+	}
+	defer resp.Body.Close()
+	decoder := json.NewDecoder(resp.Body)
+	err = decoder.Decode(&address)
+	if err != nil {
+        fmt.Println(err)
 	}
 	return address.IP
 }
 
 func getAddressIpv6() string{
-	client := resty.New()
 	address := address{}
-	resp, _  := client.R().Get("https://api6.ipify.org?format=json")
-	err := json.Unmarshal(resp.Body(), &address)
+	resp, err  := http.Get("https://api6.ipify.org?format=json")
 	if err != nil {
-        println(err)
+        fmt.Println(err)
+	}
+	defer resp.Body.Close()
+	decoder := json.NewDecoder(resp.Body)
+	err = decoder.Decode(&address)
+	if err != nil {
+        fmt.Println(err)
 	}
 	return address.IP
 }
