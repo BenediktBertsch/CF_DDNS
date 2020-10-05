dbe56ff51dd21b2b09979434dd00ca87e192e9a6
unknown <benedikt.bertsch@gmail.com>
2020-10-05 11:02:01 +0200

    quick fix for docker builds without ipv6

--- api_test.go
+++ api_test.go
@@ -1,26 +1,17 @@
 package main
 
 import (
+	"testing"
+
 	"github.com/BenediktBertsch/cf_ddns/httpclient"
-    "testing"
 )
 
 //APITest = Unit test if the ipv4 works.
 func TestGetAddressIpv4(t *testing.T) {
-    _, err := httpclient.GetAddressIpv4()
-    if err != nil {
-        t.Errorf("Failed: Could not fetch the IP String!")
-    } else {
-        t.Logf("Success: Could fetch the IP String!")
-    }
-}
-
-//APITest = Unit test if the ipv6 works.
-func TestGetAddressIpv6(t *testing.T) {
-    _, err := httpclient.GetAddressIpv6()
-    if err != nil {
-        t.Errorf("Failed: Could not fetch the IP String!")
-    } else {
-        t.Logf("Success: Could fetch the IP String!")
-    }
+	_, err := httpclient.GetAddressIpv4()
+	if err != nil {
+		t.Errorf("Failed: Could not fetch the IP String!")
+	} else {
+		t.Logf("Success: Could fetch the IP String!")
+	}
 }
